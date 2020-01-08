66bdc69524abb3c8a0c32969fd369db8f4ae7421
Francisco Luque <fluque1995@gmail.com>
2020-01-08 15:06:57 +0100

    Searching for DB for clustering

---
+++ no_supervisado/clustering/tsne_rep.py
@@ -0,0 +1,27 @@
+import numpy as np
+import sklearn.manifold
+import pandas as pd
+import plotly.express as px
+
+dataset_path = "dataset/australian.dat"
+dims = 2
+
+dataset = pd.read_csv(dataset_path, header=None, comment="@")
+
+x = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1]
+
+
+if dims == 2:
+    reduced_data = sklearn.manifold.TSNE(n_components=2).fit_transform(x)
+    df = pd.DataFrame(reduced_data, columns=["x", "y"])
+    df['labels'] = y
+    fig = px.scatter(df, x="x", y="y", color="labels", color_continuous_scale="Portland")
+
+if dims == 3:
+    reduced_data = sklearn.manifold.TSNE(n_components=3).fit_transform(x)
+    df = pd.DataFrame(reduced_data, columns=["x", "y", "z"])
+    df['labels'] = y
+    fig = px.scatter_3d(df, x="x", y="y", z="z", color="labels")
+
+fig.show()
