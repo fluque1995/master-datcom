89a0ebcd3e9463caf3556cfb18721d2c644850f0
Francisco Luque Sánchez <fluque1995@gmail.com>
2020-01-15 15:33:50 +0100

    New classification scripts

---
+++ clasificacion/laboratorio/visualizacion.R
@@ -0,0 +1,88 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(treemapify)
+
+## Preparamos los datos con los que vamos a trabajar
+datosSociales <- tibble::as_tibble(read.csv(
+                             "DatosSocialesAndalucia.csv"))
+coordenadas <- tibble::as_tibble(read.csv(
+                           "CoordenadasMunicipios.csv"))
+
+coordenadas <- dplyr::select(coordenadas, -Provincia)
+
+datos <- tibble::as_tibble(cbind(datosSociales, coordenadas))
+
+## Diagrama de barras
+ggplot2::ggplot(datos, aes(x=Provincia, y = ..count../sum(..count..)*100)) +
+    geom_bar(fill="red", color="blue") +
+    labs(y = "Porcentaje", title = "Municipos por provincia")
+
+counters <- datos %>% count(Provincia)
+
+## Ordenamos el resultado
+ggplot2::ggplot(counters, aes(x=reorder(Provincia, n), y = n)) +
+    geom_bar(stat="identity", fill="red", color="blue") +
+    geom_text(aes(label=n), vjust=-.5) +
+    labs(y = "Porcentajes", title = "Municipos por provincia")
+
+## Grafico de tarta
+contadores <- datos %>% count(Provincia) %>% arrange(desc(Provincia)) %>%
+    mutate(prop = round(n*100/sum(n), 2), laby.pos = cumsum(prop) - 0.5*prop)
+
+ggplot2::ggplot(contadores, aes(x="", y=prop, fill=Provincia)) +
+    geom_bar(width=1, stat="identity", color="black") +
+    coord_polar("y", start = 0, direction = -1) +
+    geom_text(aes(y = laby.pos, label = prop), color="black")
+
+## Mapa de árbol (librería treemapify)
+contadores <- datos %>% count(Provincia)
+
+ggplot2::ggplot(contadores, aes(fill=Provincia, area=n, label=Provincia)) +
+    geom_treemap() +
+    geom_treemap_text(color="black", place="centre") +
+    labs(title="Municipios por provincia") +
+    theme(legend.position="none")
+
+## Histograma para variables continuas
+
+ggplot2::ggplot(datos, aes(x=latitud)) +
+    geom_histogram()
+
+## Función de densidad
+ggplot2::ggplot(datos, aes(x=latitud)) +
+    geom_density()
+
+## Dos variables (ambas categóricas)
+datos <- datos %>% mutate(crece = ifelse(IncrPoblacion > 0, "si", "no"))
+
+ggplot2::ggplot(datos, aes(x=Provincia, fill=crece)) +
+    geom_bar(position = "stack")
+
+## Gráficos de puntos
+ggplot2::ggplot(datos, aes(x=longitud, y=latitud)) +
+    geom_point()
+
+datosCrecimiento <- datos %>% group_by(Provincia) %>%
+    summarize(crecMedio = mean(IncrPoblacion))
+
+ggplot2::ggplot(datosCrecimiento, aes(x=Provincia, y=crecMedio)) +
+    geom_bar(stat = "identity", fill="cornflowerblue") +
+    geom_text(aes(label = crecMedio, vjust = -.25)) +
+    labs(title = "Crecimiento medio por provincia")
+
+
+seleccionados <- dplyr::filter(datos, Provincia %in% c("Granada", "Jaén"))
+
+ggplot2::ggplot(seleccionados, aes(x=IncrPoblacion, fill=Provincia)) +
+    geom_density(alpha=.4, bins=10)
+
+ggplot2::ggplot(datos, aes(x=Provincia, y=Extranjeros)) +
+    geom_boxplot()
+
+ggplot2::ggplot(datos, aes(x=Provincia, y=Extranjeros)) +
+    geom_violin()
+
+
+ggplot2::ggplot(datos, aes(x=longitud, y = latitud, color = Provincia)) +
+    geom_point()
