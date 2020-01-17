c0641b312f2c525c8e36f2521a63dd0836ffcff3
Francisco Luque <fluque1995@gmail.com>
2020-01-17 17:34:51 +0100

    Working on kmeans. Added trial script in R

---
+++ no_supervisado/clustering/trials.R
@@ -0,0 +1,33 @@
+library(ggplot2)
+library(ggfortify)
+library(stats)
+library(fpc)
+library(cluster)
+library(dplyr)
+library(factoextra)
+library(fastDummies)
+library(knitr)
+library(Rtsne)
+
+dataset.path <- "dataset/bankloan-spss.csv"
+dataset <- read.csv(dataset.path, sep=";", dec = ",")
+dataset <- dataset[1:700,]
+
+nclust <- 2
+
+numeric.vars <- c(
+    'ingresos', 'deudaingr', 'deudacred',
+    'deudaotro', 'empleo',  'direccion', 'edad'
+)
+
+numeric.data <- dataset %>% select(numeric.vars)
+
+scaled.data <- scale(numeric.data)
+
+kmeans.res <- kmeans(numeric.data, nclust)
+
+autoplot(kmeans.res, data = numeric.data)
+
+sil <- silhouette(kmeans.res$cluster, dist(numeric.data))
+
+plot(sil, col=1:nclust)
