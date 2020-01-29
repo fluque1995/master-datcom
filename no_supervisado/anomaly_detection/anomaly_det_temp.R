19316f7d6492857b7ce9d7fc6624967e2968c39f
Francisco Luque Sánchez <fluque1995@gmail.com>
2020-01-29 15:47:12 +0100

    Working on anomaly detection

--- no_supervisado/anomaly_detection/anomaly_det_temp.R
+++ no_supervisado/anomaly_detection/anomaly_det_temp.R
@@ -1,54 +1,6 @@
-## Test de Rosner
-
-```{r}
-test.de.rosner <- rosnerTest(selected.col, 10)
-
-test.de.rosner$all.stats$Outlier
-test.de.rosner$all.stats$Obs.Num
-
-MiPlot_Univariate_Outliers(selected.col, c(218),
-                           "Outliers detectados por el test de Rosner")
-
-MiPlot_resultados_TestRosner(selected.col, 10)
-```
-
-# Test de Cerioli para el cálculo de outliers multivariados
-
-```{r}
-nivel.de.significacion = 0.05
-nivel.de.significacion.penalizado = 1 - (1 - nivel.de.significacion) ^ (1 / nrow(mis.datos.numericos))  # Transparencia 96
-
-set.seed(12)
-
-## Método de Cerioli para el cálculo de outliers
-cerioli <- cerioli2010.fsrmcd.test(
-    mis.datos.numericos, signif.alpha = nivel.de.significacion
-)
-
-cerioli$outliers
-
-is.outlier.cerioli <- which(cerioli$outliers)
-is.outlier.cerioli
-
-dist.mah.ponderadas <- cerioli$mahdist.rw
-order.idx <- order(dist.mah.ponderadas, decreasing = T)
-
-order.idx
-
-mis.datos.numericos.normalizados[order.idx[1],]
-
-## Uso del test para más de un punto anómalo
-cerioli.b <- cerioli2010.fsrmcd.test(
-    mis.datos.numericos, signif.alpha = nivel.de.significacion.penalizado
-)
-
-which(cerioli.b$outliers)
-
-```
-
 # Detección de outliers usando LOF
 
-```{r}
+``{r}
 numero.de.vecinos.lof = 5
 
 lof.scores <- lofactor(mis.datos.numericos.normalizados, numero.de.vecinos.lof)
@@ -62,24 +14,24 @@ lof.scores.ordenados
 
 plot(lof.scores.ordenados)
 
-numero.de.outliers <- 4
+numero.de.outliers <- 7
 indices.de.lof.top.outliers <- indices.segun.lof.score.ordenados[1:numero.de.outliers]
 
 is.lof.outlier <- 1:dim(
-                        mis.datos.numericos.normalizados
+                        scaled.dataset
                     )[1] %in% indices.de.lof.top.outliers
 
 is.lof.outlier
 
 MiBiPlot_Multivariate_Outliers(
-    mis.datos.numericos.normalizados, is.lof.outlier,
+    scaled.dataset, is.lof.outlier,
     "Outliers detectador por LOF"
 )
 
 data.frame.solo.outliers <- mis.datos.numericos.normalizados[is.lof.outlier,]
 
 MiBoxPlot_juntos(mis.datos.numericos.normalizados, is.lof.outlier)
-```
+``
 
 ## Uso de IQR para eliminar los outliers univariantes
 
@@ -105,7 +57,7 @@ is.lof.outlier <- 1:dim(
 
 MiBiPlot_Multivariate_Outliers(
     mis.datos.numericos.normalizados, is.lof.outlier,
-    "Outliers detectador por LOF"
+    "Outliers detectados por LOF"
 )
 
 data.frame.solo.outliers <- mis.datos.numericos.normalizados[is.lof.outlier,]
