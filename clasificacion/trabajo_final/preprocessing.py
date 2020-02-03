46d44a371055861b7350918e6f7a384ab86d1c5c
Francisco Luque Sánchez <fluque1995@gmail.com>
2020-02-03 09:45:16 +0100

    Working on classification assignment

---
+++ clasificacion/trabajo_final/preprocessing.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
+import os
+if os.path.basename(os.getcwd()) != "trabajo_final":
+    os.chdir("clasificacion/trabajo_final")
+
+dataset = pd.read_csv("dataset/train_set.csv")
+labels = pd.read_csv("dataset/train_labels.csv")
+full_dataset = pd.merge(dataset, labels)
+
+numeric_vars = full_dataset.select_dtypes(include=np.number)
+
+numeric_vars = pd.merge(numeric_vars, labels)
+numeric_vars.drop(columns="id", inplace=True)
+
+categorical_vars = full_dataset.select_dtypes(exclude=np.number)
+
+for column in numeric_vars.columns:
+    if column != "status_group":
+        sb.distplot(numeric_vars.loc[
+            numeric_vars['status_group'] == "functional", column
+        ], hist=False)
+        sb.distplot(numeric_vars.loc[
+            numeric_vars['status_group'] == "non functional", column
+        ], hist=False)
+        sb.distplot(numeric_vars.loc[
+            numeric_vars['status_group'] == "functional needs repair", column
+        ], hist=False)
+
+        plt.show()
+
+for column in categorical_vars.columns:
+    if column != "status_group":
+        bins = len(categorical_vars[column].unique())
+        if bins < 25:
+            sb.countplot(x=column, hue="status_group", data=categorical_vars)
+            plt.show()
